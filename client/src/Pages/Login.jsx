f8242b52a789018d02e4df963b7088a0efd6d446
Thejeswar <gaddamtheja@gmail.com>
2023-12-27 13:27:57 +0530

    my commit

---
+++ client/src/Pages/Login.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { Alert,Button,Form, Row,Col,Stack } from 'react-bootstrap'
+import { AuthContext } from '../Context/AuthContext'
+const Login = () => {
+    const {loginUser,loginError,updateLoginInfo,loginInfo,isloginLoading} = useContext(AuthContext)
+  return ( 
+    <>
+      <Form onSubmit={loginUser}>
+    <Row style={{
+        height:"100vh",
+        justifyContent:"center",
+        paddingTop : "6%"
+    }}>
+        <Col xs={6}>
+        <Stack gap={4}>
+            <h2>Login</h2>
+            <Form.Control type='email' placeholder='Email' onChange={(e)=>updateLoginInfo({...loginInfo,email:e.target.value})}/>
+            <Form.Control type='password' placeholder='Password' onChange={(e)=>updateLoginInfo({...loginInfo,password:e.target.value})}/>
+            <Button variant='primary' type='submit'>{isloginLoading ? "Logging.." :"Login"}</Button>
+            {loginError && <Alert variant='danger'><p>{loginError.message}</p></Alert>}
+        </Stack>
+        </Col>
+    </Row>
+   </Form> 
+    </>
+  )
+}
+
+export default Login
